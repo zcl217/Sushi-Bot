c84c2a1ba4f7df9581963693ec499b7c7693c036
Zachary Lin <zcl217@nyu.edu>
2017-02-04 10:42:09 -0500

    push

--- bot.js
+++ bot.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+/*
 var serverName = "bot testing";
 var textChannelName = "tester";
 var voiceChannelName = "General";
 var botToken = "Mjc2MjA3MTQzOTYxNzU1NjQ4.C3ORXQ.nXFQh5Ad6PP2qPktNtan6AEEpO8";
 bot.run(serverName, textChannelName, voiceChannelName, botToken);
+*/
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
